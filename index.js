b57a56afb4562401c7e02dd938f03d22a5797d6e
Zack Koppert <zkoppert@github.com>
2020-02-07 15:34:14 -0800

    Shift indentation to match vim settings
    Signed-off-by: Zack Koppert <zkoppert@github.com>

--- index.js
+++ index.js
@@ -3,18 +3,18 @@
  * @param {import('probot').Application} app
  */
 module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
+    // Your code here
+    app.log('Yay, the app was loaded!')
 
-  // Spicy response when a new issue is opened.
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue! I have alerted the office of complaints. :put_litter_in_its_place:' })
-    return context.github.issues.createComment(issueComment)
-  })
+    // Spicy response when a new issue is opened.
+    app.on('issues.opened', async context => {
+        const issueComment = context.issue({ body: 'Thanks for opening this issue! I have alerted the office of complaints. :put_litter_in_its_place:' })
+        return context.github.issues.createComment(issueComment)
+    })
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+    // For more information on building apps:
+    // https://probot.github.io/docs/
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+    // To get your app running against GitHub, see:
+    // https://probot.github.io/docs/development/
 }
