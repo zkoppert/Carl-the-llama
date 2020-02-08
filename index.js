b1a15e94b63aebda62ea50fb04afd9b24c852dc2
Zack Koppert <zkoppert@github.com>
2020-02-07 16:09:19 -0800

    Indent to match linter rules
    Signed-off-by: Zack Koppert <zkoppert@github.com>

--- index.js
+++ index.js
@@ -3,24 +3,24 @@
  * @param {import('probot').Application} app
  */
 module.exports = app => {
-    // Your code here
-    app.log('Yay, the app was loaded!')
+  // Your code here
+  app.log('Yay, the app was loaded!')
 
-    // Lint PR's
-    app.on('pull_request.opened, pull_request.updated', async context => {
-        const pullComment = context.pull_request({ body: 'This code matches the style guide. :ship:' })
-        return context.github.pull_request.createComment(pullComment)
-    })
+  // Lint PR's
+  app.on('pull_request.opened, pull_request.updated', async context => {
+    const pullComment = context.pull_request({ body: 'This code matches the style guide. :ship:' })
+    return context.github.pull_request.createComment(pullComment)
+  })
 
-    // Spicy response when a new issue is opened.
-    app.on('issues.opened', async context => {
-        const issueComment = context.issue({ body: 'Thanks for opening this issue! I have alerted the office of complaints. :put_litter_in_its_place:' })
-        return context.github.issues.createComment(issueComment)
-    })
+  // Spicy response when a new issue is opened.
+  app.on('issues.opened', async context => {
+    const issueComment = context.issue({ body: 'Thanks for opening this issue! I have alerted the office of complaints. :put_litter_in_its_place:' })
+    return context.github.issues.createComment(issueComment)
+  })
 
-    // For more information on building apps:
-    // https://probot.github.io/docs/
+  // For more information on building apps:
+  // https://probot.github.io/docs/
 
-    // To get your app running against GitHub, see:
-    // https://probot.github.io/docs/development/
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
 }
